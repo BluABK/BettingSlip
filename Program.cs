e46df7a84d4eb3a7e9ae37aeb0c177d255a88cba
BluABK <bluabk@bluabk.net>
2021-08-19 10:28:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BettingSlip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
