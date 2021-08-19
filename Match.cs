338038bcba5f3e4b24ee37542078c0098568299e
BluABK <bluabk@bluabk.net>
2021-08-19 11:01:52 +0200

    Moved match code into new class Match.

---
+++ Match.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BettingSlip
+{
+    class Match
+    {
+        private int homeGoals = 0;
+        private int awayGoals = 0;
+        private bool matchIsRunning = true;
+        private readonly string result;
+        public Match()
+        {
+            while (matchIsRunning)
+            {
+                Console.Write("Kommandoer: \r\n - H = scoring hjemmelag\r\n - B = scoring bortelag\r\n - X = kampen er ferdig\r\nAngi kommando: ");
+                var command = Console.ReadLine();
+
+                matchIsRunning = command != "X";
+                if (command == "H") homeGoals++;
+                else if (command == "B") awayGoals++;
+
+                Console.WriteLine($"Stillingen er {homeGoals}-{awayGoals}.");
+            }
+
+            // Determine result.
+            result = homeGoals == awayGoals ? "U" : homeGoals > awayGoals ? "H" : "B";
+        }
+
+        public string GetResult()
+        {
+            return result;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,29 +6,15 @@ namespace BettingSlip
     {
         static void Main(string[] args)
         {
-            var homeGoals = 0;
-            var awayGoals = 0;
-            var matchIsRunning = true;
             
             Console.Write("Gyldig tips: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nHva har du tippet for denne kampen? ");
             Console.Write("Valid tip: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nHva har du tippet for denne kampen? ");
             
             var bet = Console.ReadLine();
 
-            while (matchIsRunning)
-            {
-                Console.Write("Kommandoer: \r\n - H = scoring hjemmelag\r\n - B = scoring bortelag\r\n - X = kampen er ferdig\r\nAngi kommando: ");
-                var command = Console.ReadLine();
-            
-                matchIsRunning = command != "X";
-                if (command == "H") homeGoals++;
-                else if (command == "B") awayGoals++;
-                
-                Console.WriteLine($"Stillingen er {homeGoals}-{awayGoals}.");
-            }
+            Match match = new();
 
-            var result = homeGoals == awayGoals ? "U" : homeGoals > awayGoals ? "H" : "B";
-            var isBetCorrect = bet.Contains(result);
+            var isBetCorrect = bet.Contains(match.GetResult());
             var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
             
             Console.WriteLine($"Du tippet {isBetCorrectText}");
